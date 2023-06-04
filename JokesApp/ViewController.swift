9e568acd9f79a622048c1a9938d885b32a7621b2
Andrey Ulanov <andy.ulanov@gmail.com>
2023-06-04 21:21:09 +0400

    Initial Commit

---
+++ JokesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JokesApp
+//
+//  Created by Andrey Ulanov on 04.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
